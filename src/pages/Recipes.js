3c9449cc9d7fdba887cd47a4a67cd2d9d45c8601
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-17 23:25:55 -0700

    Search Component

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import RecipeList from '../components/RecipeList'
+import Search from '../components/Search'
+import {recipeData} from '../data/tempList'
 
 export default class Recipes extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+    state={
+        recipes:recipeData,
+        search:''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            search: e.target.value
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     render() {
         return (
-            <h4>
-                hello from recipes page
-            </h4>
+            <>
+                <Search search={this.state.search}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                >
+                </Search>
+                <RecipeList recipes={this.state.recipes}>
+
+                </RecipeList>
+            </>
         )
     }
 }
