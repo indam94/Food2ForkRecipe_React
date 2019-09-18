8a53e2a5e2207f98e271c9cdfd1d0c27898f1afe
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-17 17:51:50 -0700

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './pages/Home'
+import Recipes from './pages/Recipes'
+import SingleRecipe from './pages/SingleRecipe'
+import Default from './pages/Default'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        hello our recipe app
-      </div>
+      <Router>
+        <main>
+          {/* navbar */}
+          <Switch>
+            <Route path='/' exact component = {Home} />
+            <Route path='/recipes' exact component = {Recipes} />
+            <Route path='/recipes/:id' component = {SingleRecipe} />
+            <Route component = {Default} />
+          </Switch >
+        </main>
+        
+      </Router>
+      
     )
   }
 }
---
+++ src/pages/SingleRecipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleRecipe extends Component {
+    render() {
+        return (
+            <h4>
+                hello from single recipe page
+            </h4>
+        )
+    }
+}
